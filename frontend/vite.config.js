f8187b7bbcae1f14d8571ddfbb8b57fe26fa5154
ibioEsco <ibioing30@gmail.com>
2026-04-23 12:34:05 -0500

    subiendo a produccion el frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://assessment-360-production.up.railway.app',
         changeOrigin: true,
       },
     },
