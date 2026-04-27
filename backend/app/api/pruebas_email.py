f1be8ce9c0cd7c16f5f6f2bd352e444f09acb0f2
ibioEsco <ibioing30@gmail.com>
2026-04-27 15:57:55 -0500

    pruebas de api brevo

--- backend/app/api/pruebas_email.py
+++ backend/app/api/pruebas_email.py
@@ -1,7 +1,8 @@
 import socket
 from fastapi import APIRouter
 import os
-
+import httpx
+from fastapi import HTTPException
 
 router = APIRouter()
 
@@ -24,3 +25,26 @@ def debug_mail():
         "test_results": results,
         "tip": "Si ambos dicen Bloqueado, Gmail está rechazando el servidor de Railway."
     }
+
+
+# Endpoint para probar el envío
+@router.get("/test-send")
+async def test_send():
+    url = "https://api.brevo.com/v3/smtp/email"
+    headers = {
+        "accept": "application/json",
+        "api-key": "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", # La clave larga, no la SMTP
+        "content-type": "application/json"
+    }
+    payload = {
+        "sender": {"name": "Tu Aplicación", "email": "hello@wearequant.com"},
+        "to": [{"email": 'ibiotec30@gmail.com', "name": 'ibio'}],
+        "subject": "Prueba desde API",
+        "htmlContent": "<html><body><h1>Hola!</h1><p>Enviado vía API REST.</p></body></html>"
+    }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url, headers=headers, json=payload)
+        if response.status_code != 201:
+            raise HTTPException(status_code=400, detail=f"Error: {response.text}")
+        return {"status": "enviado"}
